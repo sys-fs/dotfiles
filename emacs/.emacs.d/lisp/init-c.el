99f4da6ef57ca1d0f72b65ea9d7cb8a4a9f744bf
Thomas Mannay <thomas@adaptive.co.uk>
2019-10-10 10:45:50 +0100

    Flycheck changes

--- emacs/.emacs.d/lisp/init-c.el
+++ emacs/.emacs.d/lisp/init-c.el
@@ -1,10 +1,13 @@
-(setq c-default-style "linux")
+;;; init-c --- C settings
+;;; Commentary:
+;;; Code:
 
 (add-to-list 'company-backends 'company-c-headers)
 
 (add-hook 'c-mode-common-hook
 	  (lambda ()
-	    (setq indent-tabs-mode t
-		  smart-tabs-mode t)))
+	    (setq indent-tabs-mode t)
+	    (c-set-style "linux")))
 
 (provide 'init-c)
+;;; init-c ends here
--- emacs/.emacs.d/lisp/init-common-lisp.el
+++ emacs/.emacs.d/lisp/init-common-lisp.el
@@ -1,7 +1,13 @@
-(setq inferior-lisp-program "/usr/bin/sbcl"
-      slime-contribs '(slime-fancy))
+;;; init-common-lisp --- Common Lisp settings
+;;; Commentary:
+;;; Code:
+
+(setq inferior-lisp-program "/usr/bin/sbcl")
+
+(slime-setup '(slime-fancy slime-company))
 
 (add-hook 'emacs-lisp-mode-hook (lambda ()
 				  (smartparens-strict-mode t)))
 
 (provide 'init-common-lisp)
+;;; init-common-lisp ends here
