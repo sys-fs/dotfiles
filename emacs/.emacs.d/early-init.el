521bafea138d41fdd156806029a503be97386e55
Thomas Mannay <tfm@disroot.org>
2023-06-26 12:07:08 +0100

    Fix repo config

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -2,8 +2,7 @@
 
 ;; Has Emacs gone too far?
 (when (eq system-type 'gnu/linux)
+  (setq tfm/release-codename (substring (shell-command-to-string "lsb_release -cs") 0 -1)
+	tfm/hashicorp-repo (concat "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com " tfm/release-codename " main"))
   (shell-command "wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg")
-  (write-region
-   "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-   nil
-   "/sudo::/etc/apt/sources.list.d/hashicorp.list"))
+  (write-region tfm/hashicorp-repo nil "/sudo::/etc/apt/sources.list.d/hashicorp.list"))
